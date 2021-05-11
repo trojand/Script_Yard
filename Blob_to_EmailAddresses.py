b71ba9955c894e1766ca606a80747fb7f7bf191a
trojand <69884213+trojand@users.noreply.github.com>
2021-05-11 18:55:47 +0400

    Update Blob_to_EmailAddresses.py

--- Blob_to_EmailAddresses.py
+++ Blob_to_EmailAddresses.py
@@ -5,13 +5,13 @@ import argparse
 parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
                                      description="DESCRIPTION:\nThis small script just filters out and prints the email address from a blob of text.\n" +
                                      "I commonly use this for filtering out email addresses from for example Office 365 / Exchange \"People\".\n" +
-                                     "In Burp suite the blob.txt would be the responses from this URL: https://outlook.office.com/owa/service.svc?action=FindPeople&app=People&n=40\n" +
-                                     "There a easier and better alternatives to this script such as Cyberchef's \"Extract Email Addresses\" Recipe (General use) or dafthack's \"Get-GlobalAddressList\" function in MailSniper (Exchange/o365 use) although processing a REALLY BIG blob of text is better using this.\n\n"+
+                                     "In Burp suite, the blob.txt would be the responses from this URL: https://outlook.office.com/owa/service.svc?action=FindPeople&app=People&n=40\n" +
+                                     "There are easier and better alternatives to this script such as Cyberchef's \"Extract Email Addresses\" Recipe (General use) or dafthack's \"Get-GlobalAddressList\" function in MailSniper (Exchange/o365 use) although processing a REALLY BIG blob of text is better using this.\n\n"+
                                      "Example commands:\n" +
                                      "$ python3 " + sys.argv[0] + " --blob blob.txt --output output.txt\n")
 
 parser.add_argument("--blob", dest="blob", required=True, type=str, help="Input file. Blob of text.")
-parser.add_argument("--output", dest="output", required=False, type=str, default="blob_email_addresses_output.txt",help="The NTDS file from which you extracted the users hashes from.\nThis could be the output of ntdsutil+secretsdump or secretsdump directly or 'crackmapexec --ntds' option, etc. Then grepped to filter out machine hashes and other non-user hash related lines.")
+parser.add_argument("--output", dest="output", required=False, type=str, default="blob_email_addresses_output.txt",help="Output file.")
 args = parser.parse_args()
 
 list_of_emails = set()
